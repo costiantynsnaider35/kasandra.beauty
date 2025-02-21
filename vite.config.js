13ed6be323c87f8a8e9aa0087d5eaf8404071b39
Ваше ім'я <constantin161089@gmail.com>
2025-02-21 17:49:33 +0200

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {},
+  },
   build: {
     sourcemap: true,
   },
