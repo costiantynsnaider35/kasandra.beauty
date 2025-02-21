af3411b625b74f02a4140872339b5b8d0615df0e
Ваше ім'я <constantin161089@gmail.com>
2025-02-21 17:57:38 +0200

    test

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -14,5 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+console.log("Firebase работает:", auth);
 console.log("Firebase API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
-console.log("Firebase Config:", firebaseConfig);
